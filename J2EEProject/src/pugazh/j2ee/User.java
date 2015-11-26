8c68d47b32f60a10d81fe292548ba50109a84cb9
pugazhendhis <pugazhendhimca@gmail.com>
2015-11-26 19:16:48 +0530

    first commit

---
+++ J2EEProject/src/pugazh/j2ee/User.java
@@ -0,0 +1,66 @@
+package pugazh.j2ee;
+
+public class User
+{
+	private String userName;
+	private String passWord;
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private String email;
+	private String mobile;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	@Override
+	public String toString()
+	{
+		 
+		 return "UserName: "+userName+" First Name: "+firstName+" second Name: "+lastName+
+				 " email: "+email+" dateofbirth: "+dateOfBirth+" Mobile: "+mobile;
+	}
+	
+	
+	
+
+}
---
+++ SpringMVC/src/puazh/springmvc/SpringMVCServlet.java
@@ -0,0 +1,12 @@
+package puazh.springmvc;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SpringMVCServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+  
+	}
+}
